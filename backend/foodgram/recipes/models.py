1bab38a0581957418ad388392d3ab864585df5b1
Your Nameelvir906 <himynameiselvir@gmail.com>
2022-05-12 17:53:04 +0500

    12.05

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from ctypes.wintypes import tagSIZE
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+User = get_user_model()
+
+
+class Recipes(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название рецепта')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор рецепта'
+    )
+    text = models.TextField(verbose_name='Рецепт приготовления')
+    cooking_time = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1)
+        ],
+        verbose_name='Время приготовления'
+    )
+    # tags
+    # image
+    # ingredients
+
+
+class Ingredients(models.Model):
+    name = models.CharField(
+        max_length=150, verbose_name='Название ингредиента'
+    )
+    quantity = models.PositiveSmallIntegerField(verbose_name='Количество')
+    measurement_unit = models.CharField(max_length=5, verbose_name='ед.изм.')
+
