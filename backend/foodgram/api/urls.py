c7f27d33c3e11cc4cad6814745b283612e16093e
Your Nameelvir906 <himynameiselvir@gmail.com>
2022-05-14 14:47:23 +0500

    1

---
+++ backend/foodgram/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from api.views import (
+    IngredientsViewSet,
+    RecipesViewSet,
+    TagsViewSet
+)
+
+router_v1 = DefaultRouter()
